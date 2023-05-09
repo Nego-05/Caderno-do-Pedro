f519aa1ed7f96d7510a667ef5c71348abb31bb3e
Nego-05 <125407854+Nego-05@users.noreply.github.com>
2023-05-09 17:04:52 -0300

    Add files via upload

---
+++ 5letra.py
@@ -0,0 +1,13 @@
+char=input("Digite uma letra")
+if char == "a":
+    print("Você digitou uma vogal")
+elif char == "e":
+    print("Você digitou uma vogal")
+elif char == "i":
+    print("Você digitou uma vogal")
+elif char == "o":
+    print("Você digitou uma vogal")
+elif char == "u":
+    print("Você digitou uma vogal")
+else:
+    print("Você digitou uma consoante")
